27a85fa13a46d16aff9ad52d5496706d9b043124
Ruri Maekawa <maekawa@komatsuelec.co.jp>
2012-05-23 11:46:05 +0900

    Initial Commit

---
+++ AlbunNavTable/RURIAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RURIAppDelegate.h
+//  AlbunNavTable
+//
+//  Created by 前川 瑠璃 on 12/05/23.
+//  Copyright (c) 2012年 小松電機産業株式会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RURIViewController;
+
+@interface RURIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) RURIViewController *viewController;
+
+@end
---
+++ AlbunNavTable/RURIViewController.h
@@ -0,0 +1,13 @@
+//
+//  RURIViewController.h
+//  AlbunNavTable
+//
+//  Created by 前川 瑠璃 on 12/05/23.
+//  Copyright (c) 2012年 小松電機産業株式会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RURIViewController : UIViewController
+
+@end
